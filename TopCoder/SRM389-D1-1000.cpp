6a54f73bbc6fcd1e65418a7a8cf3a805aa50105c
Arvind Ramaswami <saramaswami@gmail.com>
2018-05-26 09:25:40 -0700

    Create SRM389-D1-1000.cpp

---
+++ TopCoder/SRM389-D1-1000.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class LittleSquares {
+
+    int grundy[1030][1030];
+    int n;
+
+    void print(set<int> &s) {
+        for (int i : s) {
+            cout << i << " ";
+        }
+        cout << "\n";
+    }
+
+    inline int mex(set<int> &s) {
+        for (int i = 0; i <= 10000; i++) {
+            if (!s.count(i)) return i;
+        }
+        return 20000;
+    }
+
+    int solve(int top, int bottom, int n) {
+        if (~grundy[top][bottom]) return grundy[top][bottom];
+        set<int> grundys;
+        for (int i = 0; i < n; i++) {
+            if (!(top & (1 << i) ) ) { //i'th square not filled
+                grundys.insert( solve(top | (1 << i), bottom, n ));
+                if (i < n - 1) {
+                    if (!(bottom & (1 << i) ) && !(top & (1 << (i + 1)) ) && !(bottom & (1 << (1 + i)) ) ) {
+                        grundys.insert( solve(top | ((1 << i) + (1 << (i + 1))), bottom | ((1 << i) + (1 << (i + 1))), n ));
+                    }
+                }
+            }
+        }
+        grundy[top][bottom] = mex(grundys);
+        return grundy[top][bottom];
+    }
+
+    int number(string s, int n) {
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '#') res |= (1<<i);
+        }
+        return res;
+    }
+    public: 
+        string winner(vector<string> state) {
+            memset(grundy, -1, sizeof(grundy));
+            int n = state[0].size();
+            int h = state.size();
+            int g = 0;
+            for (int i = 0; i + 2 <= h; i += 2) {
+                int n1 = number(state[i], n);
+                int n2 = number(state[i + 1], n);
+                g ^= solve(n1, n2, n);
+            }
+            if (g == 0) return "second";
+            else return "first";
+        }
+
+};
